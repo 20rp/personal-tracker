435f6b5856e0cc17b55af6048c6fab609e1a1125
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2022-10-31 16:01:19 +1300

    Added login page, started working on auth logic.

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
  * Required External Modules
  */
 
-const { json } = require("express");
+const { json, res } = require("express");
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const dt = require("node-datetime");
+const mysql = require("mysql");
+const session = require("express-session");
+const { req } = require("http");
+const bp = require("body-parser");
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 
@@ -16,18 +20,34 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const app = express();
 const port = process.env.PORT || "8000";
-const file = "C:\\Users\\MG\\code\\git\\personal-tracker\\logs\\tracking.csv";
+const file = "logs\\tracking.csv";
 
 /**
  *  App Configuration
  */
 
 app.use(express.json());
-app.use(express.urlencoded( { extended: false }));
+app.use(express.urlencoded( { extended: true }));
+app.use(session({
+    secret: 'Sup3rS3cretS3r1al',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
+
+const connection = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'root',
+    password: 'theRight3ousFury!',
+    database: 'nodelogin'
+});
+
 
 const csvWriter = createCsvWriter({
     path: file,
@@ -37,7 +57,6 @@ const csvWriter = createCsvWriter({
         { id: 'urine', title: ' urination' }
     ]
 });
-
 
 function fileWriter(water, urine) {
     var dateTime = dt.create();
@@ -61,18 +80,50 @@ function tester() {
  * Routes Definitions
  */
 
+// TODO: Re-route to login page and check if user auth
 app.get("/", (req, res) => {
+
     if (fs.existsSync(file)) {
         console.log("file detected.");
     } else {
         console.log("file not detected.");
     }   
-    res.render("index", {
-        title: "Index"});
+
+    res.render("login", {
+        title: "Login"
     });
 
-app.post("/tester", (req, res) => {
-    tester();
+    console.log(req.body.username);
+});
+
+// http://localhost:3000/auth
+app.post("/auth", (req, res) => {
+
+
+    // // Ensure input fields are not left empty
+    // if (username && password) {
+    //     // Execute SQL query for login
+    //     connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+    //         // If there is an issue with the query, output the error
+    //         if (error) throw error;
+
+    //         // If the account exists
+    //         if (results.length > 0) {
+    //             // Authenticate the user
+    //             req.session.loggedIn = true;
+    //             req.session.username = username;
+
+    //             // Redirect to home page
+    //             res.redirect("/index");
+    //         } else {
+    //             res.send('Incorrect Username and Password combination.');
+    //         }
+    //         res.end();
+    //     });
+    // } else {
+    //     res.send("Please enter Username and Password");
+    //     res.end();
+    // }
 });
 
 app.post("/personal-tracker", (req, res) => {
@@ -99,5 +150,5 @@ app.post("/personal-tracker", (req, res) => {
  */
 
 app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+    console.log(`Listening to reqs on http://localhost:${port}`);
 })
